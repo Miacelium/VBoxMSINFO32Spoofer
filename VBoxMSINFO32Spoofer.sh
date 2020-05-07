2aea2ff4482de7395627a704dd602a2ce03929e4
Belph <belph@tutanota.com>
2020-05-07 13:23:10 +0200

    very basic MSINFO32 spoof
    Changes the the information in MSINFO32 of a Windows Virtual Machine in Virtual Box to look more like a real computer.
    
    Credit goes to https://github.com/Xela101/VirtualBoxSystemInformationModify for providing the batch preset I converted for this.

---
+++ VBoxMSINFO32Spoofer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+var0=`shuf -i1-32760 -n1`
+var1=`shuf -i1-32760 -n1`
+var2=`shuf -i1-32760 -n1`
+var3=`shuf -i1-32760 -n1`
+var4=`shuf -i1-32760 -n1`
+var5=`shuf -i1-32760 -n1`
+
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemVendor" "HP"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemProduct" "ENVY dv7"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemVersion" "<empty>"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemSerial" "string:$var0"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemSKU" "string:$var1"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemFamily" "<empty>"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiSystemUuid" "d18cce0e-44f9-4a74-808c-ff8ff99889da"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBIOSVendor" "HP"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBIOSVersion" "string:$var2"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBIOSReleaseDate" "%SYSdate%"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardVendor" "HP"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardProduct" "ENVY dv7"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardVersion" "string:$var3"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardSerial" "string:$var4"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardAssetTag" "string:$var5"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardLocInChass" "<empty>"
+vboxmanage setextradata "Windows 10" "VBoxInternal/Devices/pcbios/0/Config/DmiBoardBoardType" "10"
